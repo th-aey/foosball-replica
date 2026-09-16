ff1d2fbdd11617188a679d7c9a9aa9b92e69e5af
Mothae ntoi <mothae.ntoi@gmail.com>
2026-09-16 12:18:23 +0200

    Added a dockerfile

--- FoosballArena/FoosballArena/Program.cs
+++ FoosballArena/FoosballArena/Program.cs
@@ -9,6 +9,12 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddSingleton<RoomManager>();
 
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+//    options.ListenAnyIP(int.Parse(port));
+//});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
