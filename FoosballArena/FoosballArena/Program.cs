383f157b648c8b8d77e56cab24699ba7cf7c6e09
Mothae ntoi <mothae.ntoi@gmail.com>
2026-09-16 12:57:40 +0200

    changes to the program.cs

--- FoosballArena/FoosballArena/Program.cs
+++ FoosballArena/FoosballArena/Program.cs
@@ -9,12 +9,6 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddSingleton<RoomManager>();
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-    options.ListenAnyIP(int.Parse(port));
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,8 +18,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 
 app.UseAntiforgery();
